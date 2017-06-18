6eeb8a46c21a2c940c241388d7027dceeb854efb
Umair Kayani <ukayani@loyalty.com>
2017-06-18 19:12:29 -0400

    - opscenter no longer auto adds cluster, will do this from cluster node itself, so we can use the same opscenter for multiple clusters

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -3,39 +3,9 @@
 echo "Running install-datastax/bin/opscenter.sh"
 
 cloud_type=$1
-seed_nodes_dns_names=$2
-
-# Assuming only one seed is passed in for now
-seed_node_dns_name=$seed_nodes_dns_names
-
-# On GKE we resolve to a private IP.
-# On AWS and Azure this gets the public IP.
-# On GCE it resolves to a private IP that is globally routeable in GCE.
-if [[ $cloud_type == "gke" ]]; then
-  seed_node_ip=`getent hosts $seed_node_dns_name | awk '{ print $1w }'`
-elif [[ $cloud_type == "gce" ]]; then
-  # If the IP isn't up yet it will resolve to "" on GCE
-  seed_node_ip=""
-  while [ "${seed_node_ip}" == "" ]; do
-    seed_node_ip=`dig +short $seed_node_dns_name`
-  done
-elif [[ $cloud_type == "azure" ]]; then
-  # If the IP isn't up yet it will resolve to 255.255.255.255 on Azure
-  seed_node_ip="255.255.255.255"
-  while [ "${seed_node_ip}" == "255.255.255.255" ]; do
-    seed_node_ip=`dig +short $seed_node_dns_name`
-  done
-elif [[ $cloud_type == "aws" ]]; then
-  # If the IP isn't up yet it will resolve to "" on AWS?
-  seed_node_ip=""
-  while [ "${seed_node_ip}" == "" ]; do
-    seed_node_ip=`dig +short $seed_node_dns_name`
-  done
-fi
 
 echo "Configuring OpsCenter with the settings:"
 echo cloud_type \'$cloud_type\'
-echo seed_node_ip \'$seed_node_ip\'
 
 if [[ $cloud_type == "azure" ]]; then
   ./os/set_tcp_keepalive_time.sh
@@ -54,9 +24,3 @@ echo "Starting OpsCenter..."
 
 echo "Waiting for OpsCenter to start..."
 sleep 30
-
-echo "Connecting OpsCenter to the cluster..."
-./opscenter/manage_existing_cluster.sh $seed_node_ip
-
-echo "Changing the keyspace from SimpleStrategy to NetworkTopologyStrategy."
-./opscenter/configure_opscenter_keyspace.sh
